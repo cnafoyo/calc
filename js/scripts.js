992268d482f075b94ae7a7b0a2612e79bfed09de
Billy Chepkemoi <cnafayo@gmail.com>
2020-08-10 06:52:55 +0300

    html head tag setup

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-var add = function(number1, number2){
-    return number1+number2;
+var add = function (number1, number2) {
+    return number1 + number2;
 };
+var number1 = prompt("Please enter a number");
+var number2 = prompt("Please enter another number");
 
-var number1 = parseInt(prompt("Please enter a number"));
-var number2 = parseInt(prompt("Please enter another number"));
-
-alert(add(number1,number2));
+alert(add(number1, number2));
